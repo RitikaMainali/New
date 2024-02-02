6f4ecacd9e4c55e9c3468717ed59548bfdd917e1
unknown <mainaliritika@gmail.com>
2024-02-02 12:13:08 -0600

    style

--- src/App.jsx
+++ src/App.jsx
@@ -16,29 +16,28 @@ function App() {
       description: prompt('What is going on?'),
     };
 
-    setEvents([events, newEvent]);
+    setEvents([...events, newEvent]);
   };
 
   const deleteEvent = (index) => {
-    const updatedEvents = [events];
+    const updatedEvents = [...events];
     updatedEvents.splice(index, 1);
     setEvents(updatedEvents);
   };
 
   return (
     <div>
-      <h1>React Calendar App</h1>
+      <h1> Calendar </h1>
       <div>
         <Calendar onChange={onChange} value={date} />
-        <button onClick={addEvent}>Event</button>
+        <h2><button onClick={addEvent}>Event</button></h2>
       </div>
       <div>
-        <h2></h2>
         <ul>
           {events.map((event, index) => (
             <li key={index}>
               <strong>{event.date.toLocaleDateString()}</strong> - {event.description}
-              <button onClick={() => deleteEvent(index)}>Delete</button>
+              <h3><button onClick={() => deleteEvent(index)}>Delete</button></h3>
             </li>
           ))}
         </ul>
@@ -46,6 +45,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
